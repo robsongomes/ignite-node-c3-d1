3f3333e774ae89716b1be60b68cd598e847ddcbe
Robson Gomes <nosbor84@gmail.com>
2021-12-05 21:37:15 -0300

    soluçao final do desafio

--- src/modules/users/repositories/implementations/UsersRepository.ts
+++ src/modules/users/repositories/implementations/UsersRepository.ts
@@ -1,8 +1,8 @@
-import { getRepository, Repository } from 'typeorm';
+import { getRepository, Repository } from "typeorm";
 
-import { IFindUserWithGamesDTO, IFindUserByFullNameDTO } from '../../dtos';
-import { User } from '../../entities/User';
-import { IUsersRepository } from '../IUsersRepository';
+import { IFindUserWithGamesDTO, IFindUserByFullNameDTO } from "../../dtos";
+import { User } from "../../entities/User";
+import { IUsersRepository } from "../IUsersRepository";
 
 export class UsersRepository implements IUsersRepository {
   private repository: Repository<User>;
@@ -15,16 +15,30 @@ export class UsersRepository implements IUsersRepository {
     user_id,
   }: IFindUserWithGamesDTO): Promise<User> {
     // Complete usando ORM
+    const user = await this.repository
+      .createQueryBuilder("user")
+      .leftJoinAndSelect("user.games", "games")
+      .where("user.id = :id", { id: user_id })
+      .getOne();
+
+    if (!user) {
+      throw new Error("User not found");
+    }
+
+    return user;
   }
 
   async findAllUsersOrderedByFirstName(): Promise<User[]> {
-    return this.repository.query(); // Complete usando raw query
+    return this.repository.query("select * from users order by first_name"); // Complete usando raw query
   }
 
   async findUserByFullName({
     first_name,
     last_name,
   }: IFindUserByFullNameDTO): Promise<User[] | undefined> {
-    return this.repository.query(); // Complete usando raw query
+    return this.repository.query(
+      `select * from users where lower(first_name) = $1 and lower(last_name) = $2`,
+      [first_name.toLowerCase(), last_name.toLowerCase()]
+    ); // Complete usando raw query
   }
 }
